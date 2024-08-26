a146756baf572accefe107ce5d98c4e6b8a98e71
deijaparker <deijaparker1@outlook.com>
2024-08-25 21:11:44 -0700

    Implement helper functions for movie display and random selection

---
+++ helpers.js
@@ -0,0 +1,15 @@
+export const createMoviePoster = (posterPath) => {
+  // Implementation
+};
+
+export const createMovieTitle = (title) => {
+  // Implementation
+};
+
+export const createMovieOverview = (overview) => {
+  // Implementation
+};
+
+export const getRandomMovie = (movies) => {
+  // Implementation
+};
