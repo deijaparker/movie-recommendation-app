ccd1753d2215dc1e02d9ce146b9cce5dfdd05398
deijaparker <deijaparker1@outlook.com>
2024-08-25 21:08:35 -0700

    Add constants and async functions for TMDB API integration

---
+++ api.js
@@ -0,0 +1,14 @@
+export const tmdbKey = "917085bf35cc7fc201c1a56ee07d903f";
+export const tmdbBaseUrl = "https://api.themoviedb.org/3";
+
+export const getGenres = async () => {
+  // Implementation
+};
+
+export const getMovies = async () => {
+  // Implementation
+};
+
+export const getMovieInfo = async (movie) => {
+  // Implementation
+};
