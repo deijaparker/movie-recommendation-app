789109fb7d7b3ec47fc4b68040ad85102e3ec5ca
deijaparker <deijaparker1@outlook.com>
2024-08-25 21:10:17 -0700

    Import API and helper functions, and define UI-related functions

---
+++ ui.js
@@ -0,0 +1,17 @@
+import { getGenres, getMovies, getMovieInfo } from "./api.js";
+import {
+  createMoviePoster,
+  createMovieTitle,
+  createMovieOverview,
+  getRandomMovie,
+} from "./helpers.js";
+
+export const showRandomMovie = async () => {
+  // Implementation
+};
+
+export const populateGenreDropdown = (genres) => {
+  // Implementation
+};
+
+// Other UI related functions
