c2230f7776dc03d2a2538d76b6a569a662580751
deijaparker <deijaparker1@outlook.com>
2024-08-25 20:52:10 -0700

    Add function to fetch movie genres from TMDB API

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const tmdbKey = "917085bf35cc7fc201c1a56ee07d903f";
 const tmdbBaseUrl = "https://api.themoviedb.org/3";
 const playBtn = document.getElementById("playBtn");
+
+const getGenres = async () => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
